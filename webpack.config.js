d15033bd25fc92ac82e69ba9d9d1fd5d89314be2
Juan Pablo Mantica <juanpablomantica@50-204-251-125-static.hfc.comcastbusiness.net>
2019-08-17 11:57:53 -0400

    Added webpack.config for sass.

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+// include the css extraction and minification plugins
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  entry: ['./app.scss'],
+  module: {
+    rules: [
+      // compile all .scss files to plain old css
+      {
+        test: /\.(sass|scss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    // extract css into dedicated file
+    new MiniCssExtractPlugin({
+      filename: './app.css'
+    })
+  ],
+  optimization: {
+    minimizer: [
+      // enable the css minification plugin
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  }
+};

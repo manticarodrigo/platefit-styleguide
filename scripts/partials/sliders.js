4e54d82e4d9b5067fcb9799b7253418879679fb4
Rodrigo Mantica <manticarodrigo@Rodrigos-MBP.fios-router.home>
2019-09-28 18:09:14 -0400

    Finished community page.

--- scripts/partials/sliders.js
+++ scripts/partials/sliders.js
@@ -2,17 +2,29 @@ import Glide from '@glidejs/glide';
 import { getIsBreakpoint } from '../globals/utility';
 
 const onDomReady = () => {
-  const node = document.querySelector('[data-component="slider"]');
+  const sliderNodes = document.querySelectorAll('[data-component="slider"]');
 
-  if (node) {
-    const slider = new Glide('[data-component="slider"]', {
+  const getPeekSize = () => {
+    if (getIsBreakpoint('sm')) {
+      return 0;
+    }
+
+    if (getIsBreakpoint('lg')) {
+      return 50;
+    }
+
+    return 250;
+  }
+
+  [].forEach.call(sliderNodes, (node) => {
+    const slider = new Glide(node, {
       type: 'carousel',
       perView: 1,
-      peek: getIsBreakpoint('lg') ? 50 : 250,
+      peek: getPeekSize(),
     });
   
     slider.mount();
-  }
+  });
 };
 
 export default {

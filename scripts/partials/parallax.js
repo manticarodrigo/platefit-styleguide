03d1b2f52f5889aee8f0ef7e9a165efa3f065e1d
Rodrigo Mantica <manticarodrigo@Rodrigos-MBP.fios-router.home>
2019-09-28 00:43:46 -0400

    Updated locations responsive styles and added contain parallax for plate backgrounds.

--- scripts/partials/parallax.js
+++ scripts/partials/parallax.js
@@ -1,7 +1,11 @@
 import { jarallax } from 'jarallax';
 
 const onDomReady = () => {
-  jarallax(document.querySelectorAll('.bg-cover'), { speed: 0.5 });
+  const coverElements = document.querySelectorAll('[data-parallax="cover"]');
+  const containElements = document.querySelectorAll('[data-parallax="contain"]');
+
+  jarallax(coverElements, { speed: 0.5 });
+  jarallax(containElements, { speed: 0.8, imgSize: window.innerWidth > 950 ? '60%' : '90%' });
 };
 
 export default {

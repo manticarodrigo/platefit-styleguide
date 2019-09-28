7b23e46f8a31c2b1877b8a986995d45ae0801d18
Rodrigo Mantica <manticarodrigo@Rodrigos-MBP.fios-router.home>
2019-09-28 02:59:38 -0400

    Fixed contain parallax.

--- scripts/partials/parallax.js
+++ scripts/partials/parallax.js
@@ -6,7 +6,7 @@ const onDomReady = () => {
   const containElements = document.querySelectorAll('[data-parallax="contain"]');
 
   jarallax(coverElements, { speed: 0.5 });
-  jarallax(containElements, { speed: 0.8, imgSize: getIsMobile() ? '90%' : '60%' });
+  jarallax(containElements, { speed: 0.8, imgSize: getIsMobile() ? '60%' : '90%' });
 };
 
 export default {
